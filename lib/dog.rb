139f95cd9164f2ae4c43bc9a64e9d8683ae2cc81
Janice Pena <jipena94@gmail.com>
2019-05-07 04:09:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,12 +8,7 @@ class Dog
   end
 end
 
-class Dog
-  def name=(dog_name)
-    this_dogs_name = dog_name
-  end
+lassie = Dog.new
+lassie.name = "Lassie"
  
-  def name
-    this_dogs_name
-  end
-end
+lassie.name #=> "Lassie"
